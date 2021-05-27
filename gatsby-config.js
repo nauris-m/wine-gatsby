a28e4e3cb22d84d88ef120e2391ca7fba5639a4c
Nauris Mozolevs <nauris.mozolevs@visma.com>
2021-05-27 12:17:48 +0300

    * icon changed * cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,12 +19,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `winery`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fefefe`,
+        theme_color: `#fefefe`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/wine-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
 import React from "react"
-import { Link } from "gatsby"
+import {Link} from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>winery</h1>
-    <p>Welcome to winery!</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">About</Link> <br />
-    <Link to="/page-3/">Contacts</Link> <br />
-    <Link to="/using-typescript/">Some More</Link>
-  </Layout>
+    <Layout>
+        <SEO title="Home"/>
+        <h1>winery</h1>
+        <p>Welcome to winery!</p>
+        <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
+            <Image/>
+        </div>
+        <Link to="/about/">About</Link> <br/>
+        <Link to="/contacts/">Contacts</Link> <br/>
+    </Layout>
 );
 
 export default IndexPage
